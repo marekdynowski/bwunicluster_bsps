c5ce43a1806adcee1aa1808dae34f9ce7d0ba961
zrsdy01 <marek.dynowski@uni-tuebingen.de>
2015-12-03 17:28:26 +0100

    First commit
    This are simple BATCH script examples for courses for the bwUniCluster

---
+++ 02_basic_job_script_logfile/clean.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rm -fv 03_Prod.info 03_Prod.mdcrd 03_Prod.out job_uc1_*.out logfile *.log
---
+++ 05_WorkFlows/clean.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rm -rfv *.out step1.inputForStep2 step2.inputForStep3 step3.output 
